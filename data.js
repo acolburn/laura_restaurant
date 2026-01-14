eab2c24f3f4c3ff03ab2f256e8d26024633ed966
acolburn <acolburn3@gmail.com>
2026-01-13 20:29:29 -0800

    initial commit

---
+++ data.js
@@ -0,0 +1,25 @@
+export const menuArray = [
+  {
+    name: "Pu-Pu Platter",
+    price: 0.15,
+    image: "/images/pu-pu.webp",
+    description: "description text goes here",
+    id: 0,
+  },
+  {
+    name: "Tom Special",
+    price: 0.04,
+    image: "/images/bloody-mary.webp",
+    description: "description text goes here",
+
+    id: 1,
+  },
+  {
+    name: "English Muffin Pizza",
+    price: 0.06,
+    image: "/images/english-muffin-pizza.png",
+    description: "description text goes here",
+
+    id: 2,
+  },
+];
---
+++ index.js
@@ -0,0 +1,63 @@
+import { menuArray } from "./data.js";
+
+const menuDiv = document.getElementById("menu");
+const totalDiv = document.getElementById("total");
+let orders = [];
+
+document.addEventListener("click", function (event) {
+  // Only + buttons have data-name values
+  // so next line truthy only when + button clicked
+  if (event.target.dataset.name) {
+    const order = menuArray.filter(
+      (item) => item.id == event.target.dataset.name
+    );
+    // order is an array, with one object
+    // order[0] identifies the object
+    orders.push(order[0]);
+    totalDiv.classList.remove("hidden");
+  }
+  render();
+});
+
+function render() {
+  const menuHtml = menuArray
+    .map(function (menuItem) {
+      return `<div class="menu-row">
+        <img src=${menuItem.image} class="menu-img">
+        <div class="menu-item-info">
+          <div class="menu-item-name">${menuItem.name}</div>
+          <div class="menu-item-description">${menuItem.description}</div>
+          <div class="menu-item-price">${menuItem.price}</div>
+        </div>
+        <button class="btn-add" data-name="${menuItem.id}">+</button>
+      </div>
+      
+      `;
+    })
+    .join("");
+
+  menuDiv.innerHTML = menuHtml;
+
+  totalDiv.innerHTML = `<h1>Your Order</h1>`;
+  let orderTotal = 0;
+
+  let yourOrderHtml = orders
+    .map(function (orderItem) {
+      orderTotal += orderItem.price;
+      return `<div class="item-container">
+        <div class="item">${orderItem.name} <button class="btn-remove">Remove</button></div>
+        
+        <div class="price">${orderItem.price}</div>
+    </div>`;
+    })
+    .join("");
+
+  yourOrderHtml += `<hr><div class="item-container"><div class="item">Total price:</div>
+  <div class="price">${orderTotal}</div></div>`;
+
+  yourOrderHtml += `<button class="btn-order">Complete Order</button>`;
+
+  totalDiv.innerHTML += yourOrderHtml;
+}
+
+render();
