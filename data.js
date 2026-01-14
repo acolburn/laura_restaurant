78d3ee26f52865280efee8e80e100120a18d56fc
acolburn <acolburn3@gmail.com>
2026-01-13 20:34:01 -0800

    Update data.js

--- data.js
+++ data.js
@@ -2,14 +2,14 @@ export const menuArray = [
   {
     name: "Pu-Pu Platter",
     price: 0.15,
-    image: "/images/pu-pu.webp",
+    image: "./images/pu-pu.webp",
     description: "description text goes here",
     id: 0,
   },
   {
     name: "Tom Special",
     price: 0.04,
-    image: "/images/bloody-mary.webp",
+    image: "./images/bloody-mary.webp",
     description: "description text goes here",
 
     id: 1,
@@ -17,7 +17,7 @@ export const menuArray = [
   {
     name: "English Muffin Pizza",
     price: 0.06,
-    image: "/images/english-muffin-pizza.png",
+    image: "./images/english-muffin-pizza.png",
     description: "description text goes here",
 
     id: 2,
